4106121ad18a5e71f37e34916372e94de922bc26
ElChoColapIn <Etudiant@a-999878.home>
2025-07-09 17:56:14 +0200

    proute

--- src/main/java/net/mcreator/elchocolapinanyfeatures/item/CacaItem.java
+++ src/main/java/net/mcreator/elchocolapinanyfeatures/item/CacaItem.java
@@ -1,19 +1,9 @@
 package net.mcreator.elchocolapinanyfeatures.item;
 
-import net.neoforged.api.distmarker.OnlyIn;
-import net.neoforged.api.distmarker.Dist;
-
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
 
 public class CacaItem extends Item {
 	public CacaItem(Item.Properties properties) {
 		super(properties);
-	}
-
-	@Override
-	@OnlyIn(Dist.CLIENT)
-	public boolean isFoil(ItemStack itemstack) {
-		return true;
 	}
 }
